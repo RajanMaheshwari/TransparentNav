2707407788c9ef6386dffee8d316d264fe234f85
Rajan Maheshwari <rajanmaheshwari1990@gmail.com>
2015-12-14 22:30:21 +0530

    Changed NAV bar to translucent

--- TransparentNav/TransparentNav/ViewController.swift
+++ TransparentNav/TransparentNav/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let image = UIImage.imageFromColor(UIColor(red: 0.071, green: 0.071, blue: 0.922, alpha: 0.5))
+        let image = UIImage.imageFromColor(UIColor(red: 0.071, green: 0.071, blue: 0.922, alpha: 0.2))
         self.navigationController?.navigationBar.setBackgroundImage(image, forBarMetrics: UIBarMetrics.Default)
         self.navigationController?.navigationBar.barStyle = .Default
 
